c0c1a5888975d2a44c84a0ea18cabd194d403e3c
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2019-11-01 10:10:33 -0500

    Develop => High-level Technical REACT ADMIN => part 6

--- routes/Getting.js
+++ routes/Getting.js
@@ -3,14 +3,14 @@ const chalk = require('chalk');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('HOLA')
-  console.log(
-    chalk
-      .bgHex('#fb8d38')
-      .hex('#212c34')
-      .bold('=====> EMPEZAMOS <=====')
-  );
-});
+// router.get('/', (req, res) => {
+//   res.send('HOLA')
+//   console.log(
+//     chalk
+//       .bgHex('#fb8d38')
+//       .hex('#212c34')
+//       .bold('=====> EMPEZAMOS <=====')
+//   );
+// });
 
 module.exports = router;
