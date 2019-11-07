cc915e7cd33ae723be528babe50b491eadddaea5
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2019-11-07 12:00:56 -0500

    Decoration => Home>Categorias => part 3

--- client/src/components/Categorias/CategoriaItem.jsx
+++ client/src/components/Categorias/CategoriaItem.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const CategoriaItem = ({ tema }) => {
+const CategoriaItem = ({ tema, onClick }) => {
   const Item = styled.div`
     display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 25%;
+    margin: 0 6px;
     padding: 20px;
+    background-color: #fff;
+    border-radius: 4px;
+    cursor: pointer;
+    transition: all 0.2s ease-out;
+    &:hover {
+      box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
+    }
   `;
   return (
-    <Item>
+    <Item onClick={onClick}>
       <h3>{tema}</h3>
     </Item>
   );
--- client/src/components/Categorias/Categorias.jsx
+++ client/src/components/Categorias/Categorias.jsx
@@ -1,21 +1,51 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Item from './CategoriaItem';
 
 const Categorias = () => {
+  const Items = styled.div`
+    display: flex;
+  `;
+
   const items = [
-    { id: 1, tema: 'Temas' },
-    { id: 2, tema: 'Alfabetos' },
-    { id: 3, tema: 'Numeros' },
-    { id: 4, tema: 'Cordiales' },
-    { id: 5, tema: 'Notas para orientar' },
+    {
+      id: 1,
+      tema: 'Temas',
+      icon: 'https://image.flaticon.com/icons/svg/149/149330.svg',
+      page: 'temas',
+    },
+    {
+      id: 2,
+      tema: 'Alfabetos',
+      icon: 'https://image.flaticon.com/icons/svg/2219/2219077.svg',
+      page: 'alfabetos',
+    },
+    {
+      id: 3,
+      tema: 'Numeros',
+      icon: 'https://image.flaticon.com/icons/svg/106/106954.svg',
+      page: 'numeros',
+    },
+    {
+      id: 4,
+      tema: 'Cordiales',
+      icon: 'https://image.flaticon.com/icons/svg/159/159777.svg',
+      page: 'cordiales',
+    },
+    {
+      id: 5,
+      tema: 'Notas para orientar',
+      icon: 'https://image.flaticon.com/icons/svg/994/994410.svg',
+      page: 'orientar',
+    },
   ];
   return (
-    <>
+    <Items>
       {items.map(item => (
         <Item key={item.id} tema={item.tema} />
       ))}
-    </>
+    </Items>
   );
 };
 
