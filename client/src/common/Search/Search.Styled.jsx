901a2674f543903586e9bdd107f8dd5a236883d7
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2019-11-07 23:01:41 -0500

    Search => common => solution clear

---
+++ client/src/common/Search/Search.Styled.jsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const BoxSearch = styled.div`
+  position: relative;
+  display: flex;
+  width: 400px;
+`;
+
+export const SearchInp = styled.input`
+  width: 100%;
+  padding: 10px 10px 10px 38px;
+  border-top: none;
+  border-right: none;
+  border-bottom: 1px solid #ddd;
+  border-left: none;
+  border-radius: 6px;
+`;
+
+export const Icon = styled.i`
+  position: absolute;
+  top: 10px;
+  left: 8px;
+  &.bx {
+    color: #a3a9af;
+    font-size: 22px;
+  }
+`;
+export const Filter = styled.button`
+  margin-left: 10px;
+  padding: 0;
+  background-color: transparent;
+  border: none;
+  i {
+    &.bx {
+      color: #a3a9af;
+      font-size: 24px;
+      &:hover {
+        color: #134fa3;
+      }
+    }
+  }
+`;
--- client/src/common/Search/Search.jsx
+++ client/src/common/Search/Search.jsx
@@ -1,63 +1,21 @@
 import React from 'react';
-import styled from 'styled-components';
+import { BoxSearch, SearchInp, Icon, Filter } from './Search.Styled';
 
-const Search = ({ onSubmit, onChange, value, onFilter }) => {
-  const BoxSearch = styled.div`
-    position: relative;
-    display: flex;
-    width: 400px;
-  `;
-
-  const Search = styled.input`
-    width: 100%;
-    padding: 10px 10px 10px 38px;
-    border-top: none;
-    border-right: none;
-    border-bottom: 1px solid #ddd;
-    border-left: none;
-    border-radius: 6px;
-  `;
-
-  const Icon = styled.i`
-    position: absolute;
-    top: 10px;
-    left: 8px;
-    &.bx {
-      color: #a3a9af;
-      font-size: 22px;
-    }
-  `;
-  const Filter = styled.button`
-    margin-left: 10px;
-    padding: 0;
-    background-color: transparent;
-    border: none;
-    i {
-      &.bx {
-        color: #a3a9af;
-        font-size: 24px;
-        &:hover {
-          color: #134fa3;
-        }
-      }
-    }
-  `;
-  return (
-    <BoxSearch>
-      <Icon className="bx bx-search-alt" />
-      <Search
-        type="text"
-        value={value}
-        onSubmit={onSubmit}
-        onChange={onChange}
-        name="search"
-        aria-label="Buscador"
-      />
-      <Filter type="button" onClick={onFilter} aria-label="ButtonFilter">
-        <i className="bx bx-slider-alt" />
-      </Filter>
-    </BoxSearch>
-  );
-};
+const Search = ({ onSubmit, onChange, value, onFilter }) => (
+  <BoxSearch>
+    <Icon className="bx bx-search-alt" />
+    <SearchInp
+      type="text"
+      value={value}
+      onSubmit={onSubmit}
+      onChange={onChange}
+      name="search"
+      aria-label="Buscador"
+    />
+    <Filter type="button" onClick={onFilter} aria-label="ButtonFilter">
+      <i className="bx bx-slider-alt" />
+    </Filter>
+  </BoxSearch>
+);
 
 export default Search;
