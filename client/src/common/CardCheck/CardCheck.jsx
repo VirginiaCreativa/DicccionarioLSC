2bc646f29c291396368aa319dd44af877ba8b2fe
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2019-11-08 12:39:01 -0500

    Code clear Accesibilidad

--- client/src/common/CardCheck/CardCheck.jsx
+++ client/src/common/CardCheck/CardCheck.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { CardWarpper, Check, SpanCard, IconCheck } from './CardCheck.Styled';
 
-const CardCheck = ({ value, icon, onChange }) => (
+const CardCheck = ({ value, artibuto, icon, onChange }) => (
   <CardWarpper>
     <Check
       type="checkbox"
       value={value}
-      arial-label="checked"
+      aria-label={value}
+      name={artibuto}
       onChange={onChange}
     />
     <img src={icon} alt={value} />
